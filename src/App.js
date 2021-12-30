c3c8c090d5d1f0bce5343e34c90db81a8cfe19be
ROBYN13Y <yrobyn3@gmail.com>
2021-12-29 22:33:08 -0800

    UPDATE
    logo
    header
    footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./dic.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="dic-app">
+      <header>
+        <img src={logo} className="dic-icon" alt="dictionary icon" />
+        <p className="header-p">
+          Dictionary App <br />
+          <span className="header-p2">If you don't know, now you know!</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer>
+        <p>
+          <a
+            className="github-link"
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="footer-emoji">🔗</span> Open-Sourced Code
+          </a>{" "}
+          |{" "}
+          <a
+            className="ig-link"
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coded by Robyn Young <span className="footer-emoji">👩‍💻</span>
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
