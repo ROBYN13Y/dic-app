f9040bfa88c139bae4095c4bd7c314ca3c30ac76
ROBYN13Y <yrobyn3@gmail.com>
2022-01-02 17:45:28 -0800

    CREATE COMPONENT

---
+++ src/Knowledge.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Knowledge.css";
+
+export default function Knowledge(props) {
+  console.log(props.knowledge);
+
+  if (props.knowledge) {
+    return (
+      <div className="container">
+        <div className="definition">
+          <p className="knowledge-word">{props.knowledge.word}</p>
+          <p className="partOfSpeech">
+            {props.knowledge.meanings[0].partOfSpeech}
+          </p>
+          <p className="word-definition">
+            {props.knowledge.meanings[0].definitions[0].definition}
+          </p>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
