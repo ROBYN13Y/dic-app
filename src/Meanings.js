20b7d1b860db868b22bc0a90a8303d4de66a9b01
ROBYN13Y <yrobyn3@gmail.com>
2022-01-03 20:53:14 -0800

    CREATE

---
+++ src/Meanings.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Meanings.css";
+import Synonyms from "./Synonyms";
+
+export default function Meanings(props) {
+  console.log(props.meaning);
+  return (
+    <div className="meaning">
+      <p className="partOfSpeech">{props.meaning.partOfSpeech}</p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p className="word-definition">
+              <b className="def">DEFINITION:</b> {definition.definition}
+            </p>
+            <p className="word-example">{definition.example}</p>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
+      })}
+      <p className="word-definition">{props.meaning.definitions.definition}</p>
+    </div>
+  );
+}
