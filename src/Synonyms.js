94fd2b1169983e6e877e4ee16204f24a99d9356d
ROBYN13Y <yrobyn3@gmail.com>
2022-01-04 14:07:35 -0800

    UPDATE

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,14 +3,18 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  return (
-    <div className="synonyms">
-      <ul className="word-synonyms">
-        <b className="syn">SYNONYMS:</b>{" "}
-        {props.synonyms.map(function (synonyms, index) {
-          return <li key={index}>{synonyms}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  if (props.synonyms && props.synonyms.length > 0) {
+    return (
+      <div className="synonyms">
+        <ul className="word-synonyms">
+          <b className="syn">SYNONYMS:</b>{" "}
+          {props.synonyms.map(function (synonyms, index) {
+            return <li key={index}>{synonyms}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
