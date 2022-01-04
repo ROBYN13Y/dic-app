4cad344401999e4bbdbc59b5a9ed463db1c020c9
ROBYN13Y <yrobyn3@gmail.com>
2022-01-03 20:53:22 -0800

    UPDATE

--- src/Knowledge.js
+++ src/Knowledge.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Knowledge.css";
 
-import Synonyms from "./Synonyms";
+import Meanings from "./Meanings";
 
 export default function Knowledge(props) {
   // console.log(props.knowledge);
@@ -10,23 +10,14 @@ export default function Knowledge(props) {
   if (props.knowledge) {
     return (
       <div className="container knowledge">
-        <div className="word">
-          <p className="knowledge-word">{props.knowledge.word}</p>
-        </div>
-        <div className="definition">
-          <p className="partOfSpeech">
-            {props.knowledge.meanings[0].partOfSpeech}
-          </p>
-          <p className="word-definition">
-            {props.knowledge.meanings[0].definitions[0].definition}
-          </p>
-          <p className="word-example">
-            {props.knowledge.meanings[0].definitions[0].example}
-          </p>
-          <Synonyms
-            synonyms={props.knowledge.meanings[0].definitions[0].synonyms}
-          />
-        </div>
+        <p className="knowledge-word">{props.knowledge.word}</p>
+        {props.knowledge.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meanings meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
