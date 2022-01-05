232afda8fff8a54ba94636dec56d5ceb1f7aea40
ROBYN13Y <yrobyn3@gmail.com>
2022-01-04 16:10:28 -0800

    CREATE

---
+++ src/Photos.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <div className="photos">
+        {props.photos.map(function (photo, index) {
+          return (
+            <img src={photo.src.tiny} className="word-photo" width="100px" />
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
