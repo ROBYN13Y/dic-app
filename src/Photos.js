1db6fabcc7da4eba6830c4d509437eada7930a0a
ROBYN13Y <yrobyn3@gmail.com>
2022-01-04 16:25:26 -0800

    UPDATE

--- src/Photos.js
+++ src/Photos.js
@@ -4,17 +4,19 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
+    // console.log(props.photos);
     return (
       <div className="photos">
         {props.photos.map(function (photo, index) {
           return (
-            <img
-              src={photo.src.tiny}
-              className="word-photo"
-              width="100px"
-              alt="photo"
-            />
+            <div className="photo" key={index}>
+              <img
+                src={photo.src.tiny}
+                className="word-photo"
+                width="100px"
+                alt="photo"
+              />
+            </div>
           );
         })}
       </div>
