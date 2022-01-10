33e637fefba94a2586d7a82ad65fbf908ca88204
ROBYN13Y <yrobyn3@gmail.com>
2022-01-09 22:34:28 -0800

    UPDATE
    .monster1
    .monster2

--- src/Monsters.js
+++ src/Monsters.js
@@ -8,8 +8,13 @@ import "./Monsters.css";
 export default function Monsters() {
   return (
     <div>
-      <img src={monster} className="monster1" alt="monster1" />
-      <img src={monster2} className="monster2" alt="monster2" />
+      <img
+        src={monster}
+        className="monster1"
+        alt="monster1"
+        title="Oh, that is interesting!"
+      />
+      <img src={monster2} className="monster2" alt="monster2" title="Hmm...." />
     </div>
   );
 }
